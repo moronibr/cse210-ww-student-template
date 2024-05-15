a25cb79084e264f985fa9473d4383f3367f90c0f
moronibr <moroni.ramos@gmail.com>
2024-05-14 21:23:35 -0300

    Prepare Week 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+
+        WritingAssignment assignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine(assignment.GetWritingInformation());
+
+        MathAssignment math = new MathAssignment("Roberto Rodriguez", "Fractions", "frac-3-4", "frac-3-4");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+
     }
 }
